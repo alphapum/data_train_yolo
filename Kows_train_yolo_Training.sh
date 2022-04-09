0d8fe51cc1d30f93b8b303b4790559fbf7e940d1
alphapum <73872221+alphapum@users.noreply.github.com>
2022-04-09 12:38:54 +0700

    sat

---
+++ Kows_train_yolo_Training.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+#SBATCH -p dgx-preempt
+#SBATCH -N 1 
+#SBATCH -t 01:00:00
+#SBATCH -A proj0174
+#SBATCH -J KK-Yolo-Demo
+
+#SBATCH --reservation=thaisc_88
+
+
+
+
+
+module purge
+
+
+module load CUDA/11.1.1-GCC-10.2.0 
+module load cuDNN/8.0.5.39-CUDA-11.1.1
+
+cd yolov5
+
+~/miniconda3/envs/torch/bin/python train.py --epochs 100 --data ../data_train_yolo/Data.yaml  --cfg  ../yolov5s.yaml --weights ../yolov5s.pt --name expert  --img 512 --cache
+
+
+
+
+
+
+
